af63020dc4ba5767ffef210b7cdf7cf56a72fb1c
MeetSaraf <56164927+MeetSaraf@users.noreply.github.com>
2020-01-09 20:21:17 +0100

    Update logger.py

--- logger.py
+++ logger.py
@@ -6,21 +6,16 @@ import os
 from logging.handlers import RotatingFileHandler
 
 log_name = 'log_file'
-
 log_formatter = logging.Formatter(
     "%(asctime)s %(levelname)s %(funcName)s(%(lineno)d) %(message)s"
 )
-
 logFile = log_name
-
 file_handler = RotatingFileHandler(
     logFile, mode="a", maxBytes=15 * 1024 * 1024, backupCount=2, encoding=None, delay=0
 )
 file_handler.setFormatter(log_formatter)
-
 stream_handler = logging.StreamHandler()
 stream_handler.setFormatter(log_formatter)
-
 log = logging.getLogger(__name__)
 
 if "DEBUG" in os.environ:
